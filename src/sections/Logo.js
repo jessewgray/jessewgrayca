723ebefb11521500f60b7bce1d1328cf88c74af4
Jesse Gray <jwgray2@gmail.com>
2024-02-02 12:01:22 -0500

    home button update

--- src/sections/Logo.js
+++ src/sections/Logo.js
@@ -1,7 +1,10 @@
 const Logo = () => {
+    const goHome = () => {
+        window.location.href = "https://jessewgray.ca/"
+    }
     return (
         <div>
-            <img className="logo bird" src={require('../pics/neonLogo.png')} style={{width:"75px"}} />
+            <img className="logo bird" src={require('../pics/neonLogo.png')} style={{width:"75px"}} onClick={goHome}/>
         </div>
     )
 }
