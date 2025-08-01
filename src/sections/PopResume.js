8dcbc65474d385b4a37e4fe003a1bb14cb8a30fe
Jesse Gray <jwgray2@gmail.com>
2025-08-01 16:08:21 -0400

    resume update

--- src/sections/PopResume.js
+++ src/sections/PopResume.js
@@ -1,8 +1,8 @@
 
 const goToRes = () => {
     console.log('working resume')
-    // window.open('https://jessewgray.space/resume/jesseResume.pdf')
     window.open('https://jessewgray.github.io/jwgResume/')
+     // window.open('https://jessewgray.space/resume/jesseResume.pdf')
 }
 
 const PopResume = () => {
